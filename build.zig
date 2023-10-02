2beacd41912587973b4efbf96031dae7be6e3e52
Quentin_T <quentin.tessier@epitech.eu>
2023-10-02 15:35:29 +0200

    Adding dependency to zmath from the zig-gamedev submodule

--- build.zig
+++ build.zig
@@ -1,18 +1,24 @@
 const std = @import("std");
 
+const zmath = @import("extern/zig-gamedev/libs/zmath/build.zig");
+
 inline fn thisDir() []const u8 {
     return comptime std.fs.path.dirname(@src().file) orelse ".";
 }
 
 pub fn buildGame(b: *std.Build, name: []const u8, root_file: []const u8, target: std.zig.CrossTarget, optimize: std.builtin.Mode) *std.Build.CompileStep {
+    const zmath_pkg = zmath.package(b, target, optimize, .{});
     const engine = b.createModule(.{ .source_file = .{
         .path = thisDir() ++ "/src/engine/antlene.zig",
-    }, .dependencies = &.{} });
+    }, .dependencies = &.{
+        .{ .name = "zmath", .module = zmath_pkg.zmath },
+    } });
 
     const game = b.createModule(.{ .source_file = .{
         .path = root_file,
     }, .dependencies = &.{
         .{ .name = "antlene", .module = engine },
+        .{ .name = "zmath", .module = zmath_pkg.zmath },
     } });
     const exe = b.addExecutable(.{
         .name = name,
