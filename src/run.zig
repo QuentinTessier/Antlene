6af810e6ac756df3968024818effb90eeea92d3e
Quentin_T <quentin.tessier@epitech.eu>
2023-11-28 09:45:35 +0100

    cleaning

--- src/run.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-const antlene = @import("antlene");
-const game = @import("game");
-
-pub fn main() anyerror!void {
-    try antlene.entry(game.getApplicationInformation());
-}
